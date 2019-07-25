0261c76f77df32c7bdfea186b5e62fbe3b2c1544
AslanJu <53304888+AslanJu@users.noreply.github.com>
2019-07-25 19:56:54 +0600

    Update main.py

--- main.py
+++ main.py
@@ -117,17 +117,17 @@ def show_result(msg):
 
 @server.route('/' + token, methods=['POST'])
 def get_message():
-bot.process_new_updates([types.Update.de_json(flask.request.stream.read().decode("utf-8"))])
-return "!", 200
+    bot.process_new_updates([types.Update.de_json(flask.request.stream.read().decode("utf-8"))])
+    return "!", 200
 
 @server.route('/', methods=["GET"])
 def index():
-bot.remove_webhook()
-bot.set_webhook(url=f"https://{appname}.herokuapp.com/{token}")
-return "Hello from Heroku!", 200
+    bot.remove_webhook()
+    bot.set_webhook(url=f"https://{appname}.herokuapp.com/{token}")
+    return "Hello from Heroku!", 200
 
 
 if __name__ == "__main__":
-server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
 
 
